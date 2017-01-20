c20d776391a85926384da3c985b28dc6bcca5a86
PanharithHuotRealWat <panharith.huot@realwat.net>
2017-01-20 14:30:36 +0700

    Redirect to trade desk

--- www/static/js/controllers.js
+++ www/static/js/controllers.js
@@ -1,35 +1,35 @@
 var app = angular.module('myApp', ['isteven-multi-select']);
-app.config(['$interpolateProvider','$httpProvider',
-	function( $interpolateProvider,$httpProvider) {
-		$interpolateProvider.startSymbol('{$');
-		$interpolateProvider.endSymbol('$}');
-		$httpProvider.defaults.xsrfCookieName = 'csrftoken';
-		$httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-	},
+app.config(['$interpolateProvider', '$httpProvider', '$locationProvider',
+    function ($interpolateProvider, $httpProvider, $locationProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+        $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+        $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+    },
 ]);
-app.controller('myController',['$scope', '$http', function($scope, $http) {
-	// $scope.myArray = [
-	// 	{name:'item1'},
-	// 	{name:'item2'}
-	// ];
-	//
-	$scope.allJobType = [
-			{name:'item1'},
-			{name:'item2'}
-	];
-	$scope.newArray = [];
+app.controller('myController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
+    $scope.allJobType = [
+        {name: 'item1'},
+        {name: 'item2'}
+    ];
+    $scope.newArray = [];
 
-	$http({
-		method : 'GET',
-		url : 'http://192.168.2.98:9000/list/job/type/'
-	}).then(function mySucces(response) {
-		for(var i = 0 ; i < response.data.length; i++){
-			$scope.allJobType[i] = {name:response.data[i].title};
-		}
-		// $scope.allJobType = response.data;
-	}, function myError(response) {
-		$scope.datas = response.statusText;
-	});
+    $http({
+        method: 'GET',
+        url: 'http://192.168.2.98:9000/list/job/type/'
+    }).then(function mySucces(response) {
+        for (var i = 0; i < response.data.length; i++) {
+            $scope.allJobType[i] = {name: response.data[i].title};
+        }
+    }, function myError(response) {
+        $scope.datas = response.statusText;
+    });
+
+    $scope.submitForm = function () {
+        // console.log("it work" + $scope.newArray.toString());
+        $window.location.href = 'http://192.168.2.98:9000/name='+$scope.newArray[0].name;
+        // $window.location.href = 'http://192.168.2.98:9000/jobs/#/'+'name='+$scope.newArray[0].name;
+    }
 
 }]);
 
