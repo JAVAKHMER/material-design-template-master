53b7bb601a6b6fa38fcc830179a1836058071737
PanharithHuotRealWat <panharith.huot@realwat.net>
2017-01-23 16:46:31 +0700

    integrated imi with tradedesk.

--- www/static/js/controllers.js
+++ www/static/js/controllers.js
@@ -9,30 +9,34 @@ app.config(['$interpolateProvider', '$httpProvider', '$locationProvider',
 ]);
 app.controller('myController', ['$scope', '$http', '$window', function ($scope, $http, $window) {
     $scope.allJobType = [
-        {name: 'item1'},
-        {name: 'item2'}
+        {name: 'Temp to Direct',id:'1'},
+        {name: 'Temp/Full Time',id:'2'},
+        {name: 'Internship',id:'3'},
+        {name: 'Part Time',id:'4'},
+        {name: 'Freelance',id:'5'},
+        {name: 'Full Time',id:'6'},
     ];
-    $scope.newArray = [];
+    $scope.typeSelected = [];
 
-    $http({
-        method: 'GET',
-        url: 'http://192.168.2.98:9000/list/job/type/'
-    }).then(function mySucces(response) {
-        for (var i = 0; i < response.data.length; i++) {
-            $scope.allJobType[i] = {name: response.data[i].title};
-        }
-    }, function myError(response) {
-        $scope.datas = response.statusText;
-    });
+    // $http({
+    //     method: 'GET',
+    //     url: 'http://192.168.2.98:9000/list/job/type/'
+    // }).then(function mySucces(response) {
+    //     for (var i = 0; i < response.data.length; i++) {
+    //         $scope.allJobType[i] = {name: response.data[i].title};
+    //     }
+    // }, function myError(response) {
+    //     $scope.datas = response.statusText;
+    // });
 
     $scope.submitForm = function () {
         // console.log("it work" + $scope.newArray.toString());
-        $window.location.href = 'http://192.168.2.98:9000/name='+$scope.newArray[0].name;
-        // $window.location.href = 'http://192.168.2.98:9000/jobs/#/'+'name='+$scope.newArray[0].name;
+        // $window.location.href = 'http://192.168.2.98:9000/name='+$scope.newArray[0].name;
+        $window.location.href = 'http://localhost:8000/jobs/#/?'+$scope.typeSelected[0].id;
     }
 
 }]);
-
+//panharith
 //
 // var app = angular.module('myapp',[]);
 // app.config(['$interpolateProvider','$httpProvider',
